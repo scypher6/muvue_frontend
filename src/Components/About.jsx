2761ede42d84d6dfc98af79c0466c4a93eb191ec
scypher6 <scypher6@gmail.com>
2020-03-26 03:47:16 -0400

    updated about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,23 +5,22 @@ export default class About extends Component {
         return (
             <div className='about'>
                 <h1>About Muvue</h1>
-                <br />
-                <br />
-                Muvue was built using the following technologies:
-                <br />
-                <ul>
+                <h2>Muvue was built using the following technologies:</h2> 
+                <h4>
+                 <ul>
                     <li>YouTube API</li>
                     <li>React</li>
                     <li>Redux</li>
                     <li>Rails</li>
+                    <li>PostgreSQL</li>
                     <li>RestClient</li>
                     <li>ActionMailer</li>
                     <li>Auth/JWT</li>
                     <li>Figaro</li>
                     <li>Sweet Alert</li>
-                </ul>
-                <br />
-                Thank you for using Muvue!
+                 </ul>
+                </h4>
+                <h3>Thank you for using Muvue!</h3>
                 <br />
                 <br />
                 <br />
